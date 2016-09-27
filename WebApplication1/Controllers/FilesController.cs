a1a2e6f600bfb71e91a13a291dc0ca1950ee0730
zhongpeng <zhongpeng2000@gmail.com>
2016-09-27 01:09:36 -0700

    initial comit

---
+++ WebApplication1/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FileContent()
+        {
+            string id = (string)Request.RequestContext.RouteData.Values["id"];
+
+            string readFile = Properties.Resources.ResourceManager.GetString(id, Properties.Resources.Culture);
+            //readFile = readFile.Replace("\r\n", "<br />");
+            //readFile = readFile.Replace(" ", "&nbsp;");
+            return View((object)readFile);
+        }
+    }
+}
